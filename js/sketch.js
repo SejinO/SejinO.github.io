d9aba8061f0892734a6df2c1aa55e44c6bb7c4ea
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-05-18 04:29:41 +0900

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -4,70 +4,73 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+var innerwidth = innerWidth;
+var innerheight = innerHeight;
+
 c.imageSmoothingEnabled = false;
 
-console.log(canvas);
 
-var mouse = {
-    x: undefined,
-    y: undefined
+window.addEventListener('resize', resizeCanvas);
+
+function resizeCanvas() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	innerwidth = canvas.width;
+	innerheight = canvas.height;
+	animate();
 }
 
-window.addEventListener('mousemove',
-    function(event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-        console.log(mouse);
-    })
-
 function Circle(x, y, dx, dy, radius) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
 
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'rgba(0,0,255,0.7)';
-        c.stroke();
-        c.fillStyle = 'rgba(10,10,10,0.9)';
-        c.fill();
-    }
-    this.update = function() {
-        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-            this.dx = -this.dx;
-        }
-        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
-        this.x += this.dx;
-        this.y += this.dy;
+	this.draw = function () {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = 'rgba(0,0,255,0.7)';
+		c.stroke();
+		c.fillStyle = 'rgba(10,10,10,0.9)';
+		c.fill();
+	}
+	this.update = function (innerwidth, innerheight) {
+		if (this.x + this.radius > innerwidth || this.x - this.radius < 0) {
+			this.dx = -this.dx;
+		}
+		if (this.y + this.radius > innerheight || this.y - this.radius < 0) {
+			this.dy = -this.dy;
+		}
+		this.x += this.dx;
+		this.y += this.dy;
 
-        this.draw();
-    }
+		this.draw();
+	}
 }
 
 var circleArray = [];
-//init
-for (var i = 0; i < 50; i++) {
-    var x = radius + Math.random() * (innerWidth - radius * 2);
-    var y = radius + Math.random() * (innerHeight - radius * 2);
-    var dx = (Math.random() - 0.5);
-    var dy = (Math.random() - 0.5);
-    var radius = 50 * Math.random();
-    circleArray.push(new Circle(x, y, dx, dy, radius));
+
+for (var i = 0; i < 20; i++) {
+	var x = radius + Math.random() * (innerwidth - radius * 2);
+	var y = radius + Math.random() * (innerheight - radius * 2);
+	var dx = (Math.random() - 0.5);
+	var dy = (Math.random() - 0.5);
+	var radius = 50 * Math.random();
+	circleArray.push(new Circle(x, y, dx, dy, radius));
 }
+
+
+
 
 //draw
 function animate() {
-    requestAnimationFrame(animate);
-    	c.clearRect(0, 0, innerWidth, innerHeight);
-    for (var i = 0; i < circleArray.length; i++) {
-        //	circle.update();
-        circleArray[i].update();
-    }
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerwidth, innerheight);
+	for (var i = 0; i < circleArray.length; i++) {
+		//	circle.update();
+		circleArray[i].update(innerwidth, innerheight);
+	}
 }
 
 animate();
